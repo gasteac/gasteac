c9e07c6b834a98fc323327a2acc1a80e5c4e3e07
Gastón E. Acosta <gasteac@gmail.com>
2024-03-20 05:41:21 -0300

    terminado

--- ui/components/Navbar.jsx
+++ ui/components/Navbar.jsx
@@ -5,18 +5,39 @@ export const Navbar = () => {
     <>
       <div className="navbar">
         <NavLink to="/">
-          <img style={{ height: "5vh", marginTop:'10px'}} src="gasteacLogoWhite.png" alt="" />
+          <img
+            style={{ height: "5vh", marginTop: "10px" }}
+            src="gasteacLogoWhite.png"
+            alt=""
+          />
         </NavLink>
 
         <h3>
           <NavLink className="gasteacLogo" to="/home">
-          «Home🏠/»
+            «Home🏠/»
           </NavLink>
         </h3>
         <div>
           <NavLink to="/about">«About Me/»</NavLink>
           <NavLink to="/links">«Links/»</NavLink>
         </div>
+      </div>
+
+      <div className="navbar2">
+        <NavLink target="_blank" to="https://www.linkedin.com/in/gasteac/" >
+          <img
+            style={{ width: "40px", marginTop:'15px' }}
+            src="linkedin-mark-blue.png"
+            alt="linkedin"
+          />
+        </NavLink>
+        <NavLink target="_blank" to="https://github.com/gasteac">
+          <img
+            style={{ width: "40px", marginTop:'15px'  }}
+            src="github-mark-white.png"
+            alt="github"
+          />
+        </NavLink>
       </div>
     </>
   );
