30e0d7aa7aa2a5c663f3e2a6729b82564ed292c8
Gastón E. Acosta <gasteac@gmail.com>
2024-03-20 02:13:51 -0300

    started

--- ui/components/pages/AboutPage.jsx
+++ ui/components/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ export const AboutPage = () => {
   return (
     <>
     
-    <div className='pages animate__animated animate__fadeInUp animate__faster'>
+    <div className='pages animate__animated animate__fadeInDown animate__faster'>
     
     <div class="card">
     <img src="https://img.freepik.com/premium-photo/big-black-wolf-full-body-standing-mountain_690577-845.jpg" alt="gasteac" />
--- ui/components/pages/LinkPages.jsx
+++ ui/components/pages/LinkPages.jsx
@@ -3,11 +3,12 @@ import { NavLink } from "react-router-dom";
 import "animate.css";
 export const LinkPages = () => {
   return (
-    <div className="pages animate__animated animate__fadeInUp animate__faster">
+    <div className="pages animate__animated animate__fadeInDown animate__faster">
  
         <NavLink target="_blank" to="https://github.com/gasteac">
           <img
-            style={{ width: "300px" }}
+          className="links"
+            style={{ width: "300px", marginRight:'180px'}}
             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
             alt="github"
           />
@@ -15,7 +16,8 @@ export const LinkPages = () => {
    
         <NavLink target="_blank" to="https://www.linkedin.com/in/gasteac/">
           <img
-            style={{ width: "400px" }}
+          className="links"
+            style={{ width: "400px"}}
             src="https://static.vecteezy.com/system/resources/previews/023/986/980/non_2x/linkedin-logo-linkedin-logo-transparent-linkedin-icon-transparent-free-free-png.png"
             alt="linkedin"
           />
