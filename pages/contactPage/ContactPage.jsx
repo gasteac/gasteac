73120669b1873642b0a86e38b1343efa4c6d786b
Gastón E. Acosta <gasteac@gmail.com>
2024-03-25 07:45:58 -0300

    changed design

--- pages/contactPage/ContactPage.jsx
+++ pages/contactPage/ContactPage.jsx
@@ -5,9 +5,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import gasteacApi from "../../api/gasteacApi";
 
-
 export const ContactPage = () => {
-  
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -18,83 +16,92 @@ export const ContactPage = () => {
     validationSchema: Yup.object({
       name: Yup.string()
         .max(30, "Must be 30 characters or less")
-        .required("Required"),
+        .required("Required!"),
       message: Yup.string()
         .min(10, "Must be 10 characters or more")
-        .required("Required"),
-      email: Yup.string().email("Invalid email address").required("Required"),
+        .required("Required!"),
+      email: Yup.string().email("Invalid email address").required("Required!"),
     }),
-    onSubmit: async({name, email, phone= '', message}) => {
+    onSubmit: async ({ name, email, phone = "", message }) => {
       try {
-      const {data} = await gasteacApi.post('/form/new', {name: name, email: email, phone: phone, newMessage : message})
+        const { data } = await gasteacApi.post("/form/new", {
+          name: name,
+          email: email,
+          phone: phone,
+          newMessage: message,
+        });
 
-      formik.resetForm();
-      setSent(true)
+        formik.resetForm();
+        setSent(true);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      
     },
   });
-  const [sent, setSent] = useState(false)
+  const [sent, setSent] = useState(false);
   return (
-    <div className="pages animate__animated animate__fadeIn">
-      <div className="contact">
-        <div className="login-box">
-          <form onSubmit={formik.handleSubmit}>
-            <div className="user-box">
-              <input
-                name="name"
-                onChange={formik.handleChange}
-                value={formik.values.name}
-                type="text"
-                required=""
-              />
-              <div>
-
-                
-              </div>
+    <div className="contactPage">
+      <div className="login-box">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="user-box">
+            <div className="fieldRequired">
               <label htmlFor="name">Name</label>
               {formik.touched.name && formik.errors.name ? (
-                <h3 style={{marginTop:'0', color:'#FF4D4D', textAlign:'end'}}>{formik.errors.name}</h3>
+                <h6>{formik.errors.name}</h6>
               ) : null}
             </div>
-            <div className="user-box">
-              <input
-                name="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-                type="text"
-                required=""
-              />
+
+            <input
+              name="name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              type="text"
+            />
+            <div></div>
+          </div>
+          <div className="user-box">
+            <div className="fieldRequired">
               <label htmlFor="email">Email</label>
               {formik.touched.email && formik.errors.email ? (
-                <h3 style={{marginTop:'0', color:'#FF4D4D', textAlign:'end'}}>{formik.errors.email}</h3>
+                <h6>{formik.errors.email}</h6>
               ) : null}
             </div>
-            <div className="user-box">
-              <input
-                name="phone"
-                onChange={formik.handleChange}
-                value={formik.values.phone}
-                type="tel"
-                required=""
-              />
-              <label htmlFor="phone">Phone</label>
-              
-            </div>
-            <div className="user-box">
-              <textarea
-                name="message"
-                onChange={formik.handleChange}
-                value={formik.values.message}
-                required=""
-              ></textarea>
+            <input
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              type="text"
+            />
+          </div>
+          <div className="user-box">
+            <label htmlFor="phone">Phone</label>
+            <input
+              name="phone"
+              onChange={formik.handleChange}
+              value={formik.values.phone}
+            />
+          </div>
+          <div className="user-box">
+            <div className="fieldRequired">
               <label htmlFor="message">Message</label>
               {formik.touched.message && formik.errors.message ? (
-                <h3 style={{marginTop:'0', color:'#FF4D4D', textAlign:'end'}}>{formik.errors.message}</h3>
+                <h6>{formik.errors.message}</h6>
               ) : null}
             </div>
+            <textarea
+              name="message"
+              onChange={formik.handleChange}
+              value={formik.values.message}
+            ></textarea>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+         
             <button type="submit">
               <a type="submit">
                 <span></span>
@@ -104,15 +111,15 @@ export const ContactPage = () => {
                 Submit
               </a>
             </button>
-            {sent 
-            ? (<a style={{marginLeft:'120px', color:'#ECFDF9'}} id='done' className="animate__animated animate__fadeIn">
-                
+            {sent ? (
+              <p style={{color:'green'}} id="done" >
                 Sent!
-              </a>)
-          : ''}
-            
-          </form>
-        </div>
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+        </form>
       </div>
     </div>
   );
--- ui/components/Navbar/Navbar.jsx
+++ ui/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ export const Navbar = () => {
          <label htmlFor='sidebar-active' className="open-sidebar-button">
          <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
          </label>
-         <label id="overlay" for="sidebar-active"></label>
+         <label id="overlay" htmlFor="sidebar-active"></label>
          <div className="links-container">
           <label htmlFor='sidebar-active' className="close-sidebar-button">
           <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
