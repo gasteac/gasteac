665850b03482eb52a98ccb0a47dff5e4189bf152
Gastón E. Acosta <gasteac@gmail.com>
2024-03-26 02:59:51 -0300

    fixed proyects image

--- pages/proyectsPage/ProyectsPage.jsx
+++ pages/proyectsPage/ProyectsPage.jsx
@@ -8,9 +8,7 @@ export const ProyectsPage = () => {
       <div className="textProyect">
         <h1>ERROR 404</h1>
         <p>We are working on it!</p>
-
-        <p>But hey!</p>
-        <p>Look at this parallax wolf</p>
+        <p>Look this parallax wolf</p>
         <Tilt className="tilt">
           <div>
             <img src="whiteWolf.png" />
